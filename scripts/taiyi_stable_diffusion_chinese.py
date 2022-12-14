7abfe655920f3757f20f483ba565edce2d6829c4
Juncheng <liujuncheng1022@gmail.com>
2022-12-14 11:40:52 +0800

    taiyi stable diffusion chinese (#1)
    * taiyi stable diffusion chinese
    
    * remove autocast

---
+++ scripts/taiyi_stable_diffusion_chinese.py
@@ -0,0 +1,46 @@
+import os
+
+os.environ["ONEFLOW_MLIR_CSE"] = "1"
+os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
+os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
+os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
+os.environ["ONEFLOW_MLIR_GROUP_MATMUL"] = "1"
+os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
+
+os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_CONV_BIAS"] = "1"
+os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
+
+os.environ["ONEFLOW_KERENL_CONV_ENABLE_CUTLASS_IMPL"] = "1"
+os.environ["ONEFLOW_KERENL_FMHA_ENABLE_TRT_FLASH_ATTN_IMPL"] = "1"
+os.environ["ONEFLOW_KERNEL_GLU_ENABLE_DUAL_GEMM_IMPL"] = "1"
+
+os.environ["ONEFLOW_CONV_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
+
+import click
+import oneflow as torch
+from diffusers import OneFlowStableDiffusionPipeline
+from pathlib import Path
+
+
+@click.command()
+@click.option("--token", help="auth token")
+@click.option("--repeat", default=32, help="")
+@click.option("--output", default="output", help="")
+def benchmark(token, repeat, output):
+    pipe = OneFlowStableDiffusionPipeline.from_pretrained(
+        "IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v0.1",
+        use_auth_token=token,
+        torch_dtype=torch.float16,
+    )
+    pipe = pipe.to("cuda")
+    Path(output).mkdir(parents=True, exist_ok=True)
+    prompt = "飞流直下三千尺, 疑是银河落九天, 瀑布, 插画"
+    for r in range(repeat):
+        images = pipe(prompt).images
+        for i, image in enumerate(images):
+            image.save(f"output/{r}-{i}.png")
+
+
+if __name__ == "__main__":
+    benchmark()
