41d43b42ef03a7335c5b272cc2dd0a86cdc5f12d
Juncheng <liujuncheng1022@gmail.com>
2023-01-28 16:17:25 +0800

    update anything_v3_0 (#12)

--- scripts/anything_v3_0.py
+++ scripts/anything_v3_0.py
@@ -20,30 +20,55 @@ os.environ["ONEFLOW_MATMUL_ALLOW_HALF_PRECISION_ACCUMULATION"] = "1"
 
 import click
 import oneflow as torch
-from diffusers import OneFlowStableDiffusionPipeline as StableDiffusionPipeline
+from diffusers import (
+    OneFlowStableDiffusionPipeline as StableDiffusionPipeline,
+    OneFlowDPMSolverMultistepScheduler as DPMSolverMultistepScheduler,
+)
+
 from pathlib import Path
+
+DEFAULT_PROMPT = "masterpiece, best quality, illustration, beautiful detailed, finely detailed, dramatic light, intricate details, 1girl, brown hair, green eyes, colorful, autumn, cumulonimbus clouds, lighting, blue sky, falling leaves, garden"
+DEFAULT_NEGATIVE_PROMPT = "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name"
 
 
 @click.command()
 @click.option("--token")
-@click.option(
-    "--prompt",
-    default="1girl, brown hair, green eyes, colorful, autumn, cumulonimbus clouds, lighting, blue sky, falling leaves, garden",
-)
+@click.option("--prompt", default=DEFAULT_PROMPT)
+@click.option("--negative_prompt", default=DEFAULT_NEGATIVE_PROMPT)
+@click.option("--guidance_scale", default=12)
 @click.option("--repeat", default=32)
 @click.option("--output", default="output")
-def benchmark(token, prompt, repeat, output):
-    pipe = StableDiffusionPipeline.from_pretrained(
-        "Linaqruf/anything-v3.0",
-        use_auth_token=token,
-        revision="diffusers",
-        torch_dtype=torch.float16,
-    )
+@click.option("--height", default=640)
+@click.option("--width", default=512)
+@click.option("--num_inference_steps", default=50)
+def benchmark(
+    token,
+    prompt,
+    negative_prompt,
+    guidance_scale,
+    repeat,
+    output,
+    height,
+    width,
+    num_inference_steps,
+):
+    model_id = "Linaqruf/anything-v3.0"
+
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe = pipe.to("cuda")
+
     output_dir = Path(output).joinpath("anything_v3_0")
     output_dir.mkdir(parents=True, exist_ok=True)
     for r in range(repeat):
-        images = pipe(prompt).images
+        images = pipe(
+            prompt,
+            negative_prompt=negative_prompt,
+            height=height,
+            width=width,
+            guidance_scale=guidance_scale,
+            num_inference_steps=50,
+        ).images
         for i, image in enumerate(images):
             image.save(output_dir.joinpath(f"{r:03d}-{i:02d}.png"))
 
