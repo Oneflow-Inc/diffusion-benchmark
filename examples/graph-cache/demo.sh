79939fadc2898618fe995f415f21430102a8c810
jackalcooper <jackalcooper@gmail.com>
2023-02-02 12:17:49 +0800

    add

--- examples/graph-cache/demo.sh
+++ examples/graph-cache/demo.sh
@@ -1,4 +1,4 @@
 set -e
 export ONEFLOW_NNGRAPH_ENABLE_PROGRESS_BAR=1
 time python3 examples/graph-cache/infer.py --save
-time python3 examples/graph-cache/infer.py --laod
+time python3 examples/graph-cache/infer.py --load
